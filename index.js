39c391345bc5d409327ac619642c8de165b2879b
Andrey Dodonov <aadodonov@gmail.com>
2021-03-12 10:41:54 +0100

    Get user info first

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const CNetilionAPI = require('./CNetilionAPI.js');
 
 const apiClient = new CNetilionAPI(process.env.USER, process.env.PASS);
 
+apiClient.UserAPI.getCurrentUser()
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+
 apiClient.AssetAPI.getAssets()
     .then((data) => {
         console.log(data);
